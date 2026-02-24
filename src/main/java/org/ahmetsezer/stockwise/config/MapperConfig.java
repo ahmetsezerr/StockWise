4e77e0db4b76e877ea436d48a1f9215d40e1a126
ahmetsezerr <115064937+ahmetsezerr@users.noreply.github.com>
2026-02-24 15:15:31 +0300

    feat: enhance ModelMapper configuration and add custom Product mapping

--- src/main/java/org/ahmetsezer/stockwise/config/MapperConfig.java
+++ src/main/java/org/ahmetsezer/stockwise/config/MapperConfig.java
@@ -1,5 +1,7 @@
 package org.ahmetsezer.stockwise.config;
 
+import org.ahmetsezer.stockwise.dto.response.ProductResponse;
+import org.ahmetsezer.stockwise.entity.Product;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
@@ -10,12 +12,22 @@ public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
+
         ModelMapper modelMapper = new ModelMapper();
 
         modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.STRICT) 
+                .setMatchingStrategy(MatchingStrategies.STRICT)
                 .setFieldMatchingEnabled(true)
                 .setSkipNullEnabled(true);
+
+
+        modelMapper.typeMap(Product.class, ProductResponse.class)
+                .addMappings(mapper ->
+                        mapper.map(
+                                src -> src.getCategory().getName(),
+                                ProductResponse::setCategoryName
+                        )
+                );
 
         return modelMapper;
     }
